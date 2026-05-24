ecb78460d0820728156ca2b969ceb318f124f0af
Chase Badalato <chasebad8@hotmail.com>
2026-05-24 13:28:17 -0400

    added initial opcode list

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "cpu.h"
+#include "cpu_opcodes.h"
 #include "common/logging.h"
+
+/* this can be global, its read only */
+static opcode_handler_t opcode_table[OP_MAX]     = { 0 };
+static bool             opcode_table_initialized = false;
+
+static void op_nop(cpu_t *cpu, uint8_t opcode)
+{
+   LOG_DEBUG("opcode %0X NOP", opcode);
+}
+
+static void op_unimplemented(cpu_t *cpu, uint8_t opcode)
+{
+   LOG_WARN("opcode 0x%0X is not implemented", opcode);
+
+#ifdef EXIT_ON_BAD_OPCODE
+   exit(-1);
+#endif
+}
+
+static void cpu_bind_opcodes(void)
+{
+   if(opcode_table_initialized == false)
+   {
+      for(uint8_t opcode = 0; opcode < OP_MAX; opcode++)
+      {
+         opcode_table[opcode] = op_unimplemented;
+      }
+
+      opcode_table[OP_NOP] = op_nop;
+   }
+
+   opcode_table_initialized = true;
+}
 
 void cpu_init(cpu_t *cpu, bus_t *bus)
 {
+   LOG_DEBUG("initializing cpu ...");
+
    cpu->A = 0;
    cpu->B = 0;
    cpu->C = 0;
@@ -17,6 +54,11 @@ void cpu_init(cpu_t *cpu, bus_t *bus)
    cpu->SP = 0;
 
    cpu->bus = bus;
+
+   cpu_bind_opcodes();
+
+   LOG_DEBUG("cpu init success!");
+
 }
 
 void cpu_step(cpu_t *cpu)
@@ -30,26 +72,8 @@ void cpu_step(cpu_t *cpu)
    else
    {
       /* fetch */
-      LOG_DEBUG("FETCH");
       uint8_t opcode = bus_read(cpu->bus, cpu->PC++);
-      LOG_DEBUG("opcode: 0x%0X", opcode);
-
-      /*decode and execute */
-      LOG_DEBUG("DECODE & EXECUTE");
-      switch(opcode)
-      {
-         case 0x00:
-            LOG_DEBUG("NO OP");
-            break;
-         default:
-            LOG_WARN("opcode not implemented");
-            break;
-      }
-
-      if(opcode != 0x0)
-      {
-         exit(1);
-      }
+      opcode_table[opcode](cpu, opcode);
    }
 }
 
--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -21,6 +21,8 @@ typedef struct
 
 } cpu_t;
 
+typedef void (*opcode_handler_t)(cpu_t *cpu, uint8_t opcode);
+
 void cpu_init(cpu_t *cpu, bus_t *bus);
 
 void cpu_step(cpu_t *cpu);
